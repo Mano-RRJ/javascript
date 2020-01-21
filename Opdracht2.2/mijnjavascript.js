557697282e7f72b3a44eb4953895dea13a656e43
Mano <rtorbed@roc-dev.com>
2020-01-21 11:00:46 +0100

    2.2 af

---
+++ Opdracht2.2/mijnjavascript.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerHTML = "Dit is de titel pagina";
+document.getElementById("subkop").innerHTML = "Dit is de subkop";
+document.getElementById("aap1").src = "img/aap.jpg";
+document.getElementById("aap2").src = "img/gekke aap.jpg";
